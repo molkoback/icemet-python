45918bdb3bf0e732ed3253ff93549736f5aab818
molkoback <molkoback@gmail.com>
2020-06-30 09:56:48 +0300

    improved config class

--- icemet/__init__.py
+++ icemet/__init__.py
@@ -1,6 +1,6 @@
 import os
 import tempfile
 
-version = "1.4.0"
+version = "1.4.1"
 cache = os.path.join(tempfile.gettempdir(), "icemet")
 os.makedirs(cache, exist_ok=True)
--- icemet/cfg.py
+++ icemet/cfg.py
@@ -4,10 +4,27 @@ class ConfigException(Exception):
 	pass
 
 class Config:
+	def __init__(self, fn):
+		self.file = fn
+		self.dict = {}
+		self.read(fn)
+	
+	def __setitem__(self, k, v):
+		self.dict[k] = v
+	
+	def __getitem__(self, k):
+		return self.dict.get(k, None)
+	
 	def read(self, fn):
-		with open(fn, "r") as fp:
-			self.dict = yaml.load(fp, Loader=yaml.Loader)
+		try:
+			with open(fn, "r") as fp:
+				self.set_dict(yaml.load(fp, Loader=yaml.Loader))
+		except Exception as e:
+			raise ConfigException("Couldn't parse config file '{}'\n{}".format(fn, e))
 	
 	def write(self, fn):
 		with open(fn, "w") as fp:
 			yaml.dump(self.dict, fp, Dumper=yaml.Dumper)
+	
+	def set_dict(self, dict):
+		self.dict = dict
