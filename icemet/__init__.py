bcfc5091a7f96a5509ef117e7b76bf396e0ecb2f
molkoback <molkoback@gmail.com>
2020-07-01 18:24:04 +0300

    delete cache at exit

--- icemet/__init__.py
+++ icemet/__init__.py
@@ -1,6 +1,10 @@
+import atexit
 import os
+import shutil
 import tempfile
 
-version = "1.4.1"
+version = "1.4.2"
 cache = os.path.join(tempfile.gettempdir(), "icemet")
+
 os.makedirs(cache, exist_ok=True)
+atexit.register(lambda : shutil.rmtree(cache))
