6b7879707ca09d313f420623d6e7aa65a6b49712
molko <molkoback@gmail.com>
2020-11-03 16:14:43 +0200

    individual cache dirs

--- icemet/__init__.py
+++ icemet/__init__.py
@@ -2,9 +2,10 @@ import atexit
 import os
 import shutil
 import tempfile
+import time
 
-version = "1.5.0"
-cache = os.path.join(tempfile.gettempdir(), "icemet")
+version = "1.5.1"
+cache = os.path.join(tempfile.gettempdir(), "icemet", "{:x}".format(int(time.time()*1000)))
 
-os.makedirs(cache, exist_ok=True)
+os.makedirs(cache)
 atexit.register(lambda : shutil.rmtree(cache, ignore_errors=True))
