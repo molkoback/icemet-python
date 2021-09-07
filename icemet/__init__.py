e772281ee02ed718b6ead02ce38a2ab65a771bd7
molko <molkoback@gmail.com>
2021-09-07 12:44:39 +0300

    release 1.6.0

--- icemet/__init__.py
+++ icemet/__init__.py
@@ -4,7 +4,7 @@ import shutil
 import tempfile
 import time
 
-version = "1.6.0-dev"
+version = "1.6.0"
 cache = os.path.join(tempfile.gettempdir(), "icemet", "{:x}".format(int(time.time()*1000)))
 
 os.makedirs(cache)
